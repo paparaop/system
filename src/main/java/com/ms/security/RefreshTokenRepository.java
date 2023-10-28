485957bb6c6112db7b39f46d0291bcfa7ab2efc7
paparao.pandiri <paparao.pandiri@affiliate.ansr.com>
2023-10-28 12:42:41 +0530

    jwt security added

---
+++ src/main/java/com/ms/security/RefreshTokenRepository.java
@@ -0,0 +1,26 @@
+package com.ms.security;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.repository.CrudRepository;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.util.Optional;
+
+import static io.micronaut.data.model.query.builder.sql.Dialect.H2;
+
+@JdbcRepository(dialect = H2) // <1>
+public interface RefreshTokenRepository extends CrudRepository<RefreshTokenEntity, Long> { // <2>
+
+    @Transactional
+    RefreshTokenEntity save(@NonNull @NotBlank String username,
+                            @NonNull @NotBlank String refreshToken,
+                            @NonNull @NotNull Boolean revoked); // <3>
+
+    Optional<RefreshTokenEntity> findByRefreshToken(@NonNull @NotBlank String refreshToken); // <4>
+
+    long updateByUsername(@NonNull @NotBlank String username,
+                          boolean revoked); // <5>
+}
